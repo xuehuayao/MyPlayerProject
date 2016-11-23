518a2035e8a412f74cbaaad70a194c305982bb8c
yaoxuehua <xuehuayao@leedian.com>
2016-11-23 14:36:18 +0800

    添加部分注释

--- app/src/main/java/com/example/yaoxuehua/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yaoxuehua/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.yaoxuehua.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 音乐播放器主页面
+ * */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
